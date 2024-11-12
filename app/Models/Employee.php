3837e0433df8a628a5364225748b3b7f3dacfe7a
BeniKovac <beni.kovac@gmail.com>
2024-11-12 14:37:33 +0100

    setup models

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    //
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = ['name', 'surname', 'position_id', 'department_id'];
+
+    public function scopeBasicInfo($query)
+    {
+        return $query->with(['department', 'position']);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
 }

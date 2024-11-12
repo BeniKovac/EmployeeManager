aa5a26b18ae2c982c8f45cb947e466ee4f2e3cf9
BeniKovac <beni.kovac@gmail.com>
2024-11-12 14:39:23 +0100

    added actual numbers to statistics in dashboard

--- resources/js/Components/PageDashboard.tsx
+++ resources/js/Components/PageDashboard.tsx
@@ -3,35 +3,39 @@ import DataTable from './DataTable'
 import '../../css/PageDashboard.css'
 
 interface Props {
-  recentEmployees: any[]
+  averageEmploymentTime: number
+  recentEmployees: any[],
+  totalDepartments: number,
+  totalEmployees: number,
+  totalThisMonthEmployees: number
 }
 
-const PageDashboard = ({ recentEmployees }: Props) => {
+const PageDashboard = ({ averageEmploymentTime, recentEmployees, totalDepartments, totalEmployees, totalThisMonthEmployees }: Props) => {
   return (
     <>
       <div className="flex mt-4 gap-4">
         <div className="flex-1 bg-white shadow-lg rounded-lg p-6">
           <h1 className="text-xl font-semibold mb-4 text-gray-500">Total Employees</h1>
-          <b>X</b>
+          <b>{totalEmployees}</b>
         </div>
         <div className="flex-1 bg-white shadow-lg rounded-lg p-6">
           <h1 className="text-xl font-semibold mb-4 text-gray-500">Departments</h1>
-          <b>Y</b>
+          <b>{totalDepartments}</b>
         </div>
         <div className="flex-1 bg-white shadow-lg rounded-lg p-6">
           <h1 className="text-xl font-semibold mb-4 text-gray-500">New this month</h1>
-          <b>Z</b>
+          <b>{totalThisMonthEmployees}</b>
         </div>
         <div className="flex-1 bg-white shadow-lg rounded-lg p-6">
-          <h1 className="text-xl font-semibold mb-4 text-gray-500">Average employment time</h1>
-          <b>V</b>
+          <h1 className="text-xl font-semibold mb-4 text-gray-500">Average employment time (days)</h1>
+          <b>{averageEmploymentTime}</b>
         </div>
       </div>
 
       <div className="flex mt-4 gap-4 custom-right-margin">
         <div className="flex-1 bg-white shadow-lg rounded-lg p-6">
           <h1 className="text-xl font-semibold mb-4 text-black">Recent Employees</h1>
-          <DataTable data={recentEmployees} displayedFields={['id', 'name', 'position', 'department']}></DataTable>
+          <DataTable data={recentEmployees} displayedFields={['id', 'name', 'position.position_title', 'department.department_title', 'employment_date']}></DataTable>
         </div>
         <div className="flex-1"></div>
       </div>
